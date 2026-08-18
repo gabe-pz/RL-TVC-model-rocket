65287dab07b2eed0013f85d71101ce2933c6879f
Gabe Perez <gperez58857@gmail.com>
2026-08-18 09:57:58 -0700

    removed old pid contorl files

--- include/control/pidControl.h
+++
@@ -1,8 +0,0 @@
-#pragma once 
-
-#include <array> 
-#include <algorithm>
-
-#include "../math/rocketMath.h"
-
-void pidControl(std::array<double, 3>& pidArray, const std::array<double, 3>& pidGains, double& prevError, double& desiredAngle, double rocketAngle, double dt, int axis);
